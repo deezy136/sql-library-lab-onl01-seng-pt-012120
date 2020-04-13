b6ad26d491a07f82603b4876bc7d85d6d871e971
Clyde Griffin <deezy13@gmail.com>
2020-04-13 01:17:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,8 +31,7 @@ def select_series_title_with_most_human_characters
   'SELECT series.title 
   FROM series 
   JOIN characters ON series.id = characters.series_id 
-  WHERE characters.species = "human"
-  LIMIT 1;'
+  WHERE characters.species = "human";'
 end
 
 def select_character_names_and_number_of_books_they_are_in
