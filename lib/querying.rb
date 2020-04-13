ecfeba34bacca6d22c77c6351fe8d7a96ba786fe
Clyde Griffin <deezy13@gmail.com>
2020-04-13 01:22:55 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
- p ' SELECT series.title 
+ ' SELECT series.title 
  FROM series 
  JOIN books 
  ON series.id = books.series_id 
